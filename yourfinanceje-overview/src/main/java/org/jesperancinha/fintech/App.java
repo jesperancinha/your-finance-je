0c992fec688b36a890819592bf62f92545a033bf
Joao Esperancinha <jofisaes@gmail.com>
2020-03-13 08:19:32 +0100

    Reestructuring an old App and bringing it back to the Future

---
+++ yourfinanceje-overview/src/main/java/org/jesperancinha/fintech/App.java
@@ -0,0 +1,10 @@
+package org.jesperancinha.fintech;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
