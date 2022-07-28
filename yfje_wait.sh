62432d9056e1111abedd4716e11a405cf2fd6e67
Joao Esperancinha <jofisaes@gmail.com>
2022-07-28 20:25:09 +0200

    Wait script to wait

--- yfje_wait.sh
+++ yfje_wait.sh
@@ -3,19 +3,20 @@
 function checkServiceByNameAndMessage() {
     name=$1
     message=$2
-    docker-compose logs "$name" &> "logs"
+    docker-compose logs "$name" > "logs"
     string=$(cat logs)
     counter=0
     while [[ "$string" != *"$message"* ]]
     do
       printf "."
-      docker-compose logs "$name" &> "logs"
+      docker-compose logs "$name" > "logs"
       string=$(cat logs)
       sleep 1
       counter=$((counter+1))
       if [ $counter -eq 200 ]; then
-          echo "Failed after $counter tries! Cypress tests mail fail!!"
-          exit
+          echo "Failed after $counter tries! Cypress tests may fail!!"
+          echo "$string"
+          exit 1
       fi
     done
     counter=$((counter+1))
